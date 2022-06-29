47d70b0e0544794554286b8f5bb94c45905d76cf
eko.amanov <eko.amanov@gmail.com>
2022-06-29 17:30:32 +0400

    ./

--- DebtTracking.Infrastrucuture/Data/Context/DebtTrackingContext.cs
+++ DebtTracking.Infrastrucuture/Data/Context/DebtTrackingContext.cs
@@ -2,13 +2,58 @@
 
 namespace DebtTracking.Infrastrucuture.Data.Context
 {
-    public class DebtTrackingContext : DbContext
+    public partial class DebtTrackingContext : DbContext
     {
-        public DebtTrackingContext(DbContextOptions<DebtTrackingContext> options) : base(options)
+        public DebtTrackingContext()
         {
-
         }
 
-        public DbSet<Core.Entities.Customer> Customers { get; set; }
+        public DebtTrackingContext(DbContextOptions<DebtTrackingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Test> Tests { get; set; } = null!;
+        public virtual DbSet<Test2> Test2s { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Data Source=172.18.102.22,1535;Initial Catalog=Lisi;Persist Security Info=True;User ID=sa_lisi;Password=oavshetrix");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Test>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("test", "dt");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(10)
+                    .HasColumnName("id")
+                    .IsFixedLength();
+            });
+
+            modelBuilder.Entity<Test2>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("test2", "dt");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(10)
+                    .HasColumnName("id")
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
